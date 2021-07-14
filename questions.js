88f8edc8cb74a0a86f1e6fa4b66b1eb8d753ab67
Menal Kour <74865342+MenalKour@users.noreply.github.com>
2021-07-14 22:59:52 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,122 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Lemon Is Hybrid Of?",
+    answer: "Bitter Orange And Citron",
+    options: [
+      "Orange And Lime",
+      "Bitter Orange And Citron",
+      "Sweet Orange And Citron",
+      "None Of The Above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Statue Of Liberty Is Gifted To USA By Which Country?",
+    answer: "France",
+    options: [
+      "France",
+      "India",
+      "Italy",
+      "Russia"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which Is The Smallest Ocean?",
+    answer: "Arctic Ocean",
+    options: [
+      "Pacific Ocean",
+      "Indian Ocean",
+      "Atlantic Ocean",
+      "Arctic Ocean"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which Was The Largest Snake In The History Of The World?",
+    answer: "Titanoboa Cerrejonensis",
+    options: [
+      "Anaconda",
+      "Python",
+      "Titanoboa Cerrejonensis",
+      "King Cobra"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Dead Sea Is Located Between Which Two Countries?",
+    answer: "Jordan And Israel",
+    options: [
+      "Jordan And Sudan",
+      "Jordan And Israel",
+      "Turkey And UAE",
+      "UAE And Egypt"
+    ]
+  },
+ 
+  
+
+   {
+    numb: 6,
+    question: "In Which Ocean 'Bermunda Triangle' Region Is Located?",
+    answer: "Atlantic",
+     options: [
+       "Indian",
+       "Pacific",
+       "Atlantic",
+       "Arctic"
+    ]
+   },
+
+
+   {
+    numb: 7,
+    question: "Which Country Is Known As The 'Playground Of Europe'?",
+    answer: "Switzerland",
+     options: [
+       "Austia",
+       "Switzerland",
+       "Holland",
+       "Italy"
+    ]
+   },
+   
+   {
+    numb: 8,
+    question: "Which Country Is Known As The 'Land Of ThunderBolts'?",
+    answer: "Bhutan",
+     options: [
+       "Bhutan",
+       "China",
+       "Venenzula",
+       "Thailand"
+    ]
+   },
+
+   {
+    numb: 9,
+    question: "Which Continent Has The Highest Number Of Countries?",
+    answer: "Africa",
+     options: [
+       "Asia",
+       "Europe",
+       "Africa",
+       "North America"
+    ]
+   },
+
+   {
+    numb: 10,
+    question: "Which Country Has White Elephants?",
+    answer: "Thailand",
+     options: [
+       "Thailand",
+       "Sri Lanka",
+       "Malasiya",
+       "India"
+    ]
+   },
+   
+];
